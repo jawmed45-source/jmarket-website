238c5839226246f77f83da5532b004a1ad8d94dd
Jawad <jawmed45@gmail.com>
2026-05-23 03:00:55 +0500

    Made Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,8 +2,158 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      <h1 className='text-black'></h1>
+    <div className='h-screen w-screen overflow-x-hidden bg-black '>
+      <div>
+        <div className=' lg:pt-10 h-[76vh] w-full lg:flex flex lg:flex-row flex-col justify-center lg:justify-start lg:relative'>
+        <div className='hidden lg:block lg:w-[20vw] bg-black lg:absolute lg:font-[font2] font-[font2] lg:text-xl lg:p-10  text-center lg:top-[39vh]'>
+          <h1 className='fadein transition opacity-0 animate-[fadein_0.6s_ease_forwards] ' 
+          style={{ animationDelay: '1.5s' }} >On a screen or a desk. <br />
+            At your place. At our place.
+            Everywhere.</h1>
+        </div>
+        <div className='lg:p-0 p-10 lg:absolute lg:left-[20%] lg:w-[60vw] text-center lg:text-[12vw] text-6xl lg:leading-[9.5vw] lg:tracking-wider uppercase'>
+          <h1 className='fadein transition opacity-0 animate-[fadein_0.6s_ease_forwards] ' 
+          style={{ animationDelay: '1.5s' }} >share <br /> Your <br /> world</h1>
+        </div>
+        <div className='block lg:hidden w-full text-center pb-10 px-[20vw] '>
+          <h1 className='fadein transition opacity-0 animate-[fadein_0.6s_ease_forwards] ' 
+          style={{ animationDelay: '1.5s' }} >On a screen or a desk. <br />
+            At your place. At our place.
+            Everywhere.</h1>
+        </div>
+        <div className=' lg:w-[20vw] bg-black lg:absolute font-[font2] lg:text-xl lg:p-10  text-center lg:top-[39vh] lg:right-0'>
+          <h1 className='fadein transition opacity-0 animate-[fadein_0.6s_ease_forwards] ' 
+          style={{ animationDelay: '1.5s' }} >House No 51/1 <br />
+            Near TCF School. <br />
+            Karachi.</h1>
+        </div>
+
+      </div>
+      <div className='fadein transition opacity-0 animate-[fadein_0.6s_ease_forwards] text-black bg-[#D3FD50] hover:bg-white transition cursor-pointer lg:h-48 h-18 overflow-hidden flex items-center'
+      style={{ animationDelay: '2s' }}>
+        <div  className='contact whitespace-nowrap flex'>
+          <h2 className=' font-[font2] text-[8vw] uppercase font-semibold '
+          >
+            Contact@jmarket.agency &nbsp;&nbsp;&nbsp;★&nbsp;&nbsp;&nbsp; Contact@jmarket.agency &nbsp;&nbsp;&nbsp;★&nbsp;&nbsp;&nbsp;
+          </h2>
+        </div>
+
+
+      </div>
+      </div>
+
+
+
+
+      <div className='pt-20'>
+        <div className=' h-[76vh] w-full lg:flex flex lg:flex-row flex-col justify-center lg:justify-start lg:relative'>
+        <div className='hidden lg:block lg:w-[20vw] bg-black lg:absolute lg:font-[font2] font-[font2] lg:text-xl lg:p-10  text-center lg:top-[39vh]'>
+          <h1 >On a screen or a desk. <br />
+            At your place. At our place.
+            Everywhere.</h1>
+        </div>
+        <div className='lg:p-0 p-10 lg:absolute lg:left-[20%] lg:w-[60vw] text-center lg:text-[12vw] text-6xl lg:leading-[9.5vw] lg:tracking-wider uppercase'>
+          <h1>share <br /> Your <br /> story</h1>
+        </div>
+        <div className='block lg:hidden w-full text-center pb-10 px-[20vw] '>
+          <h1 >On a screen or a desk. <br />
+            At your place. At our place.
+            Everywhere.</h1>
+        </div>
+        <div className=' lg:w-[20vw] bg-black lg:absolute font-[font2] lg:text-xl lg:p-10  text-center lg:top-[39vh] lg:right-0'>
+          <h1 >House No 51/1 <br />
+            Near TCF School. <br />
+            Karachi.</h1>
+        </div>
+
+      </div>
+      <div className='text-black bg-[#D3FD50] hover:bg-white transition cursor-pointer lg:h-48 h-15 overflow-hidden flex items-center'>
+        <div  className='contact whitespace-nowrap flex'>
+          <h2 className='font-[font2] text-[8vw] uppercase font-semibold'>
+            Contact@jmarket.agency &nbsp;&nbsp;&nbsp;★&nbsp;&nbsp;&nbsp; Contact@jmarket.agency &nbsp;&nbsp;&nbsp;★&nbsp;&nbsp;&nbsp;
+          </h2>
+        </div>
+
+
+      </div>
+      </div>
+
+
+
+
+      <div className='pt-20'>
+        <div className=' h-[76vh] w-full lg:flex flex lg:flex-row flex-col justify-center lg:justify-start lg:relative'>
+        <div className='hidden lg:block lg:w-[20vw] bg-black lg:absolute lg:font-[font2] font-[font2] lg:text-xl lg:p-10  text-center lg:top-[39vh]'>
+          <h1 >On a screen or a desk. <br />
+            At your place. At our place.
+            Everywhere.</h1>
+        </div>
+        <div className='lg:p-0 p-10 lg:absolute lg:left-[20%] lg:w-[60vw] text-center lg:text-[12vw] text-6xl lg:leading-[9.5vw] lg:tracking-wider uppercase'>
+          <h1>what's <br /> Your <br /> Vision?</h1>
+        </div>
+        <div className='block lg:hidden w-full text-center pb-10 px-[20vw] '>
+          <h1 >On a screen or a desk. <br />
+            At your place. At our place.
+            Everywhere.</h1>
+        </div>
+        <div className=' lg:w-[20vw] bg-black lg:absolute font-[font2] lg:text-xl lg:p-10  text-center lg:top-[39vh] lg:right-0'>
+          <h1 >House No 51/1 <br />
+            Near TCF School. <br />
+            Karachi.</h1>
+        </div>
+
+      </div>
+      <div className='text-black bg-[#D3FD50] hover:bg-white transition cursor-pointer lg:h-48 h-15 overflow-hidden flex items-center'>
+        <div  className='contact whitespace-nowrap flex'>
+          <h2 className='font-[font2] text-[8vw] uppercase font-semibold'>
+            Contact@jmarket.agency &nbsp;&nbsp;&nbsp;★&nbsp;&nbsp;&nbsp; Contact@jmarket.agency &nbsp;&nbsp;&nbsp;★&nbsp;&nbsp;&nbsp;
+          </h2>
+        </div>
+
+
+      </div>
+      </div>
+
+
+      <div className='pt-20'>
+        <div className=' h-[76vh] w-full lg:flex flex lg:flex-row flex-col justify-center lg:justify-start lg:relative'>
+        <div className='hidden lg:block lg:w-[20vw] bg-black lg:absolute lg:font-[font2] font-[font2] lg:text-xl lg:p-10  text-center lg:top-[39vh]'>
+          <h1 >On a screen or a desk. <br />
+            At your place. At our place.
+            Everywhere.</h1>
+        </div>
+        <div className='lg:p-0 p-10 lg:absolute lg:left-[20%] lg:w-[60vw] text-center lg:text-[12vw] text-6xl lg:leading-[9.5vw] lg:tracking-wide uppercase'>
+          <h1>we <br /> build <br /> together</h1>
+        </div>
+        <div className='block lg:hidden w-full text-center pb-10 px-[20vw] '>
+          <h1 >On a screen or a desk. <br />
+            At your place. At our place.
+            Everywhere.</h1>
+        </div>
+        <div className=' lg:w-[20vw] bg-black lg:absolute font-[font2] lg:text-xl lg:p-10  text-center lg:top-[39vh] lg:right-0'>
+          <h1 >House No 51/1 <br />
+            Near TCF School. <br />
+            Karachi.</h1>
+        </div>
+
+      </div>
+      <div className='text-black bg-[#D3FD50] hover:bg-white transition cursor-pointer lg:h-48 h-15 overflow-hidden flex items-center'>
+        <div  className='contact whitespace-nowrap flex'>
+          <h2 className='font-[font2] text-[8vw] uppercase font-semibold'>
+            Contact@jmarket.agency &nbsp;&nbsp;&nbsp;★&nbsp;&nbsp;&nbsp; Contact@jmarket.agency &nbsp;&nbsp;&nbsp;★&nbsp;&nbsp;&nbsp;
+          </h2>
+        </div>
+
+
+      </div>
+      </div>
+
+
+
+
+
+
+
     </div>
   )
 }
